4187264ccb07422280aa8b26533e31135b358c9b
Ali Haider <39668516+alihaider0012@users.noreply.github.com>
2018-09-27 17:48:03 +0500

    adding js and css external files

---
+++ myJS.js
@@ -0,0 +1,36 @@
+
+		function openForm() {
+			document.getElementById("myForm").style.display = "block";
+		}
+
+		function closeForm() {
+			document.getElementById("myForm").style.display = "none";
+		}
+
+		$(document).ready(function(){
+    		$("#flip").click(function(){
+        		$("#panel").slideToggle("slow");
+    		});
+		});
+		
+		$(document).ready(function(){
+    		$("#flip1").click(function(){
+        		$("#panel1").slideToggle("slow");
+    		});
+		});
+
+		function startTime() {
+			var today = new Date();
+			var h = today.getHours();
+			var m = today.getMinutes();
+			var s = today.getSeconds();
+			m = checkTime(m);
+			s = checkTime(s);
+			document.getElementById('txt').innerHTML =
+			h + ":" + m + ":" + s;
+			var t = setTimeout(startTime, 500);
+		}
+		function checkTime(i) {
+			if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+			return i;
+		}
